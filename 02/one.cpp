ec4087f740f14208a1d46f5f2460f4e16d3d7375
abhaykumar-0285 <abhay.kr2803@gmail.com>
2024-10-15 21:34:25 +0530

    done c  ++

---
+++ 02/one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+
+    cout<<"enter the first no: ";
+    cin>>a;
+    cout<<"enter the second no: ";
+    cin>>b;
+
+    cout<<"difference: "<<a-b <<endl;
+    cout<<"product: "<<a*b <<endl;
+    cout<<"divide: "<<a/b <<endl;
+    cout<<"remainder: "<<a%b <<endl;
+    return 0;
+}
